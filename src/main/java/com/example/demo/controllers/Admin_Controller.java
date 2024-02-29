efda5195c1205475d04b3d7846caf92d9023f157
Rusalka21 <gonzalesm.sergio@gmail.com>
2024-02-29 13:53:14 -0500

    CRUD cliente implementado

--- src/main/java/com/example/demo/controllers/Admin_Controller.java
+++ src/main/java/com/example/demo/controllers/Admin_Controller.java
@@ -1,12 +1,25 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Cliente;
+import com.example.demo.service.ClienteService;
 
 @Controller
 @RequestMapping("/admin")
 public class Admin_Controller {
+	
+	@Autowired
+	private ClienteService clienteService;
 
     @GetMapping("/admininicio")
     public String mostrarAdminInicio() {
@@ -14,7 +27,12 @@ public class Admin_Controller {
     }
 
     @GetMapping("/admingestionclientes")
-    public String mostrarAdminGestionClientes() {
+    public String mostrarAdminGestionClientes(Model model) {
+    	
+    	List<Cliente> listCliente = clienteService.getAllCliente();
+    	
+    	model.addAttribute("clientes" ,listCliente);
+    	
         return "AdminGestionClientes";
     }
 
@@ -47,10 +65,82 @@ public class Admin_Controller {
     public String mostrarAdClienteAdd() {
         return "AdClienteAdd";
     }
+    
+    @PostMapping("/adclienteadd")
+    public String createCliente(@RequestParam(name = "nombre") String nombre,
+    		@RequestParam(name= "apellidos") String apellidos,
+    		@RequestParam(name = "dni") Long dni,
+    		@RequestParam(name= "email") String email,
+    		@RequestParam(name = "direccion") String direccion,
+    		@RequestParam(name = "celular") Long celular,
+    		Model model) {
+    	
+    	Cliente cliente = new Cliente();
+    	
+    	cliente.setNombre(nombre);
+    	cliente.setApellidos(apellidos);
+    	cliente.setDni(dni);
+    	cliente.setEmail(email);
+    	cliente.setDireccion(direccion);
+    	cliente.setCelular(celular);
+    	
+    	clienteService.createCliente(cliente);
+    	
+    	List<Cliente> listCliente = clienteService.getAllCliente();
+    	
+    	model.addAttribute("clientes" ,listCliente);    	
+    	
+    	return "AdminGestionClientes";
+    }
 
-    @GetMapping("/adclienteedit")
-    public String mostrarAdClienteEdit() {
+    @GetMapping("/adclienteedit/{cod_cliente}")
+    public String mostrarAdClienteEdit(@PathVariable int cod_cliente, Model model) {
+    	    	    	
+    	Cliente cliente = clienteService.getClienteById(cod_cliente);
+    	
+    	model.addAttribute("cliente" ,cliente);    	
+    	
         return "AdClienteEdit";
+    }
+    
+    @PostMapping("/adclienteedit")
+    public String editCliente(@RequestParam(name = "cod_cliente") int cod_cliente,
+    		@RequestParam(name = "nombre") String nombre,
+    		@RequestParam(name= "apellidos") String apellidos,
+    		@RequestParam(name = "dni") Long dni,
+    		@RequestParam(name= "email") String email,
+    		@RequestParam(name = "direccion") String direccion,
+    		@RequestParam(name = "celular") Long celular,
+    		Model model) {
+    	
+    	Cliente cliente = clienteService.getClienteById(cod_cliente);
+    	
+    	cliente.setNombre(nombre);
+    	cliente.setApellidos(apellidos);
+    	cliente.setDni(dni);
+    	cliente.setEmail(email);
+    	cliente.setDireccion(direccion);
+    	cliente.setCelular(celular);
+    	
+    	clienteService.createCliente(cliente);
+    	
+    	List<Cliente> listCliente = clienteService.getAllCliente();
+    	
+    	model.addAttribute("clientes" ,listCliente);     	
+    	
+    	return "AdminGestionClientes";
+    }
+    
+    @GetMapping("/deletecliente/{cod_cliente}")
+    public String deleteCliente(@PathVariable int cod_cliente, Model model) {
+    	
+    	clienteService.deleteCliente(cod_cliente);
+    	
+    	List<Cliente> listCliente = clienteService.getAllCliente();
+    	
+    	model.addAttribute("clientes", listCliente);
+    	
+    	return "AdminGestionClientes";
     }
 
     @GetMapping("/usuariosadd")
