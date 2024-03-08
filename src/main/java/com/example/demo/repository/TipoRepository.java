a0f427d7ea82a9c48c4af9685031cd102d0e4329
TeresaBaquerizo <i202311085@cibetec.edu.pe>
2024-03-07 19:23:17 -0500

    upload project

--- src/main/java/com/example/demo/repository/TipoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Tipo;
-
-public interface TipoRepository extends JpaRepository<Tipo, Integer> {
-
-}
--- src/main/java/com/example/demo/repository/UnidadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Unidad;
-
-
-public interface UnidadRepository extends JpaRepository<Unidad, Integer>{
-
-}
